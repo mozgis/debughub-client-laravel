237a4819f0082b461780e0b5829a91584ea0f6a8
Janis Mozgis <janismozgis@gmail.com>
2017-04-27 23:41:32 +0300

    fix config

--- config/debughub.php
+++ config/debughub.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'api_key' => '6mi5CLqXeedC3XxAlkCAwaSFJQfd4Ycz',
-    'project_key' => 'GQQFNEMRWQVCCTGUMTGXPDNSSZOC',
+    'api_key' => '',
+    'project_key' => '',
     'endpoint' => 'http://api.debughub.com/api/v1/log/',
     'git_root' => '',
     'blacklist_params' => [
