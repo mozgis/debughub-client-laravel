de9e9c067613ab772f1b5593970d3e464318f94a
Janis Mozgis <janismozgis@gmail.com>
2017-05-29 23:36:46 +0300

    #9 fix autoload issue

--- src/Handlers/ResponseHandler.php
+++ src/Handlers/ResponseHandler.php
@@ -1,61 +1,51 @@
 <?php
+
 namespace Debughub\Client\Handlers;
 
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\View;
+use Debughub\Client\Reportable;
 use Debughub\Client\Config;
 
-class LaravelResponseHandler extends ResponseHandler
+
+class ResponseHandler implements Reportable
 {
+    public $response;
+    public $views;
+    public $headers;
+    public $gitBranchName;
+    public $response_code;
+    private $config;
 
-    private $app;
-
-    public function __construct(Config $config, Application $app)
+    public function __construct(Config $config)
     {
-        parent::__construct($config);
-        $this->app = $app;
-
-
-//        listener for view events
-        $app['events']->listen('composing:*', function ($view = null, $data = null) {
-            if (is_a($view, \Illuminate\View\View::class)){
-                $this->parseView($view);
-            } else {
-                $this->parseView($data);
-            }
-        });
-
-        $this->response = '';
-        $app['events']->listen('Illuminate\Foundation\Http\Events\RequestHandled', function ($event = null, $data = null) {
-            $request = $data[0];
-            $maxLength = 1000;
-            if (strlen($request->response->getContent()) > $maxLength) {
-              $data = substr($request->response->getContent(), 0, $maxLength);
-            } else {
-              $data = $request->response->getContent();
-            }
-            $this->response = $data;
-        });
+        $this->config = $config;
     }
 
 
-    private function parseView($data)
+
+    public function getData()
     {
-        if (is_a($data, \Illuminate\View\View::class)) {
-            $this->views[] = [
-                'name' => $data->getName(),
-                'path' => $data->getPath()
-            ];
+        $this->getGitBranchName();
 
-        }
-        if (is_array($data) && isset($data[0]) && is_a($data, \Illuminate\View\View::class)){
-          $this->views[] = [
-            'name' => $data[0]->getName(),
-            'path' => $data[0]->getPath()
-          ];
-
-        }
-
+        return [
+            'response' => $this->response,
+            'views' => $this->views,
+            'headers' => headers_list(),
+            'git_branch_name' => $this->gitBranchName,
+            'response_code' => http_response_code(),
+        ];
     }
+
+
+    protected function getGitBranchName()
+    {
+        $shellOutput = [];
+        exec('git branch | ' . "grep ' * '", $shellOutput);
+        foreach ($shellOutput as $line) {
+            if (strpos($line, '* ') !== false) {
+                $this->gitBranchName = trim(strtolower(str_replace('* ', '', $line)));
+            }
+        }
+        $this->gitBranchName = null;
+    }
+
 }
