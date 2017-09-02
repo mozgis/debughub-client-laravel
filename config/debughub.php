59a2d09d76e8906d41a6ef9b3f80ebb9a9abc671
Janis Mozgis <janismozgis@gmail.com>
2017-09-02 22:12:57 +0300

    update to https

--- config/debughub.php
+++ config/debughub.php
@@ -2,7 +2,7 @@
 return [
     'api_key' => '',
     'project_key' => '',
-    'endpoint' => 'http://api.debughub.com/api/v1/log/',
+    'endpoint' => 'https://api.debughub.com/api/v1/log/',
     'git_root' => '',
     'enabled' => false,
     'blacklist_params' => [
